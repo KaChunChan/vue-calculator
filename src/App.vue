2356b516afa3e67764c108f62c438576b1d938c5
KaChunChan <kachun.chan@outlook.com>
2021-08-09 18:21:15 +0100

    Emulate as close as possible:
    - Emulate button press add and equal buttons
    - Add in subtract button and update equal button for subtract
    - Add in multiplication button
    - Add in negate and percentage button
    - Add in division button

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #a3c313;
+  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
